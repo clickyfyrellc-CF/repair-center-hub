a18448ecbacac26bec0b67f7b2866b569427ed91
Sarfaraz Akram <miansarfaraz206905@gmail.com>
2026-08-07 16:00:42 +0500

    Updated contact information

--- lib/contact.ts
+++ lib/contact.ts
@@ -1,6 +1,6 @@
-export const whatsappNumber = '971562843310';
+export const whatsappNumber = '971527315207';
 export const whatsappLink = `https://wa.me/${whatsappNumber}`;
 
-export const phoneNumber = '971562843310';
+export const phoneNumber = '971527315207';
 export const phoneLink = `tel:+${phoneNumber}`;
-export const phoneDisplay = '056 284 3310';
+export const phoneDisplay = '052 731 5207';
